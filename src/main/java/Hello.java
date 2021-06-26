c7d5f1e1d82be53f5b850efa22b95aa51d57921f
marialopez2357 <maariia.mlg@hotmail.com>
2021-06-26 23:51:05 +0200

    addeed more work

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,5 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Developing hello feature");
+        System.out.println("github repo is already published");
+        System.out.println("doing more work");
     }
 }
